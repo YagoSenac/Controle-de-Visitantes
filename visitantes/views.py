0a790634055c7865273beb0af5d5e8ddd31ff3d1
Yago Johann <yago53863026@edum.pi.senac.br>
2024-10-03 15:32:51 -0300

    Feat: Login required fixed

--- visitantes/views.py
+++ visitantes/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from visitantes.forms import VisitanteForm
 from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def registrar_visitante(request):
     form = VisitanteForm()
     
